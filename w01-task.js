511b8f56888391440acc672b83ac1ef9495b460d
stan581994 <stan581994@gmail.com>
2024-01-24 06:06:54 +0800

    added new

--- w01-task.js
+++ w01-task.js
@@ -5,7 +5,7 @@ userName = "Moronihah";
 console.log(`Username: ${userName}`);
 
 // 🔍 Part 2 error
-const currentDateAndTime = Date();
+const currentDateAndTime = new Date();
 console.log(`It is now ${currentDateAndTime}`);
 
 // 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
