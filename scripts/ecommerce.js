d7c064c47f17a85c572ec7abe8b6c6ad9c557bdc
stan581994 <stan581994@gmail.com>
2024-02-12 21:28:51 +0800

    final commit for final project

--- scripts/ecommerce.js
+++ scripts/ecommerce.js
@@ -1,5 +1,8 @@
 const productUrl = "https://fakestoreapi.com/products/";
 
+
+let totalPrice = 0;
+
 async function fetchProduct (){
     try{
         const response = await fetch(productUrl);
@@ -21,7 +24,7 @@ const displayProduct = (productList) => {
     productList.forEach((product,index) => {
         // Create a product container
         const productContainer = document.createElement('div');
-        productsContainer.classList.add('product');
+        productContainer.classList.add('product');
 
         // Create an image element
         const imgElement = document.createElement('img');
@@ -34,16 +37,19 @@ const displayProduct = (productList) => {
         // Create an h2 element for the product name
         const h2Element = document.createElement('h2');
         h2Element.textContent = product.title;
+        h2Element.classList.add('product-title');
         productContainer.appendChild(h2Element);
 
         // Create a p element for the product description
         const pElement = document.createElement('p');
         pElement.textContent = product.description;
+        pElement.classList.add('product-description');
         productContainer.appendChild(pElement);
 
         // Create a span element for the product price
         const spanElement = document.createElement('span');
         spanElement.textContent = `$${product.price.toFixed(2)}`;
+        spanElement.classList.add('product-price');
         productContainer.appendChild(spanElement);
 
         
@@ -55,11 +61,15 @@ const displayProduct = (productList) => {
         inputElement.min = 1;
         inputElement.value = 1;
         inputElement.step = 1;
+        inputElement.classList.add('product-quantity');
         productContainer.appendChild(inputElement);
 
         // Create a button for adding to cart
         const buttonElement = document.createElement('button');
         buttonElement.textContent = 'Add to Cart';
+        buttonElement.addEventListener('click',()=> {
+            addToCart(product,inputElement.value);
+        });
         productContainer.appendChild(buttonElement);
 
         // Append the product container to the products container
@@ -68,6 +78,49 @@ const displayProduct = (productList) => {
     });
 
 }
+
+const addToCart = (product,quantity)=>{
+
+    totalPrice += product.price * quantity;
+    console.log(totalPrice);
+    const cartList = document.getElementById('cart-list');
+    const newRow = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    nameCell.textContent = product.title;
+    const qualityCell = document.createElement('td');
+    qualityCell.textContent = quantity;
+    const priceCell = document.createElement('td');
+    priceCell.textContent=product.price;
+
+    newRow.appendChild(nameCell);
+    newRow.appendChild(qualityCell);
+    newRow.appendChild(priceCell);
+    cartList.appendChild(newRow);
+      
+}
+
+const compute = () =>{
+    const checkBoxMember = document.getElementById('prime-member');
+    const totalPriceLabel = document.getElementById('total-cost');
+    const selectedValue = checkBoxMember.value;
+
+    if(selectedValue === 'yes'){
+        totalPriceLabel.textContent = `${discountedPrice(totalPrice).toFixed(2)}`;
+    }else if (selectedValue === 'no'){
+        totalPriceLabel.textContent = `${totalPrice.toFixed(2)}`;
+ 
+    }else{
+        console.log('Invalid Selection');
+    }
+
+}
+
+function discountedPrice(totalPrice){
+    return totalPrice * 0.85;
+}
+
+const computeButton = document.querySelector('#compute-total-btn').addEventListener('click',compute);
+
 
 async function init(){
     const productList = await fetchProduct();
